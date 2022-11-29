5bcd4895ff9c88e4099590274f3441329d518bd8
jfbarahonag <jfbarahonag@unal.edu.co>
2022-11-28 23:55:42 -0500

    component was cleaned after tests

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,9 +1,8 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
   
-  const [counter, setCounter] = useState(10);
 
   useEffect( ()=> {
     getGifs(category);
@@ -14,8 +13,6 @@ export const GifGrid = ({ category }) => {
     <>
       <h3>{category}</h3>
       <p>{'Hola mundo'}</p>
-      <button onClick={ () => setCounter(counter+1) }>+1</button>
-      <div>{counter}</div>
     </>
   )
 }
