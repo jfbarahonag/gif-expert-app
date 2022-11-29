1640470176af31bae107b1b1742387051a4caac3
jfbarahonag <jfbarahonag@unal.edu.co>
2022-11-28 20:36:10 -0500

    a new state was added to handle categories

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['Dead pool', 'Dragon ball']);
+
   return (
     <>
-    {/* title */}
+      {/* title */}
       <div>GifExpertApp</div>
-    {/* input */}
-    
-    {/* GIF list */}
+      {/* input */}
+
+      {/* GIF list */}
+      <ol>
+        {categories.map((category, index) => (
+          <li key={index}>
+            {category}
+          </li>
+        ))}
+      </ol>
       {/* GIF Item */}
     </>
   )
