d514ef8384380b4509c95857ae3ee8c304eabaa6
Juan Felipe Barahona Gonzalez <jbarahona@XM.com.co>
2022-11-29 14:13:53 -0500

    logic to show Loading message while is fetching the gifs was added

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -2,21 +2,21 @@ import { useFetchGifs } from "../hooks/useFetchGifs";
 import { GifCard } from "./GifCard";
 
 export const GifGrid = ({ category }) => {
-
   const { gifs, isLoading } = useFetchGifs(category);
 
   return (
     <>
-      <h3>{category}</h3>
-      <div className="card-grid">
-        {
-          gifs.map( ( {id, ...content} ) => (
-            <GifCard key={id}
-              {...content}
-            />
-          ))
-        }
-      </div>
+      {isLoading && <h1>Cargando...</h1>}
+      {
+        <>
+          <h3>{category}</h3>
+          <div className="card-grid">
+            {gifs.map(({ id, ...content }) => (
+              <GifCard key={id} {...content} />
+            ))}
+          </div>
+        </>
+      }
     </>
-  )
-}
+  );
+};
