8cc441cbe3b5de38dd57cda7ec6597164bd1d60e
jfbarahonag <jfbarahonag@unal.edu.co>
2022-11-28 20:49:52 -0500

    callback to add categories when a button is pressed was implemented

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,13 +4,23 @@ export const GifExpertApp = () => {
 
   const [categories, setCategories] = useState(['Dead pool', 'Dragon ball']);
 
+  const onAddCategory = () => {
+    const categoryTitle = 'New category';
+    setCategories(prevCategories => [...prevCategories, categoryTitle]);
+  }
+
   return (
     <>
       {/* title */}
-      <div>GifExpertApp</div>
+      <h1>GifExpertApp</h1>
       {/* input */}
 
       {/* GIF list */}
+      <button
+        onClick={onAddCategory}
+      >
+        Agregar categoría
+      </button>
       <ol>
         {categories.map((category, index) => (
           <li key={index}>
