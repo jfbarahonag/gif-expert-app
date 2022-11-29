7b11488878cae255c8e256a486f93b85d3434cd5
jfbarahonag <jfbarahonag@unal.edu.co>
2022-11-28 23:38:49 -0500

    useEffect was added to avoid query a GIFs always he component is updated

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,13 +1,21 @@
+import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
   
-  getGifs(category);
+  const [counter, setCounter] = useState(10);
+
+  useEffect( ()=> {
+    getGifs(category);
+  }, []);
+
 
   return (
     <>
       <h3>{category}</h3>
       <p>{'Hola mundo'}</p>
+      <button onClick={ () => setCounter(counter+1) }>+1</button>
+      <div>{counter}</div>
     </>
   )
 }
