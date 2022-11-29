4d0ebe04e5140f47df0fb44c45dad458ef4cc33b
jfbarahonag <jfbarahonag@unal.edu.co>
2022-11-29 00:16:59 -0500

    listing all gifs gotten by giphy was implemented

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,18 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
+
+  const [gifs, setGifs] = useState([]);
   
+  const getGifsObjects = async () => {
+    const gifObjects = await getGifs(category);
+    setGifs(gifObjects);
+  }
 
   useEffect( ()=> {
-    getGifs(category);
+    getGifsObjects();
   }, []);
 
 
   return (
     <>
       <h3>{category}</h3>
-      <p>{'Hola mundo'}</p>
+      <ol>
+        {
+          gifs.map( ({ id, title, url }) => (
+            <li key={id}>
+              <h2>{ title }</h2>
+              <img src={url} alt="" />
+            </li>
+          ))
+        }
+      </ol>
     </>
   )
 }
