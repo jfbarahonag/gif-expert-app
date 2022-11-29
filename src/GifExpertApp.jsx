970fc1cbd7975d4fd44618aa19f131f2d3fbcc28
jfbarahonag <jfbarahonag@unal.edu.co>
2022-11-28 20:27:28 -0500

    styles were added

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,7 +2,12 @@
 export const GifExpertApp = () => {
   return (
     <>
+    {/* title */}
       <div>GifExpertApp</div>
+    {/* input */}
+    
+    {/* GIF list */}
+      {/* GIF Item */}
     </>
   )
 }
